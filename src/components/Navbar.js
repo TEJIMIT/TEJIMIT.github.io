f26fdcc32674372e436866e013e43b15aed4b025
Richard Kim <kimr@mit.edu>
2018-07-08 09:38:23 -0400

    Add Navbar Toggle Button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,35 +4,65 @@ import Link from 'gatsby-link'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.png'
 
-const Navbar = () => (
-  <nav className="navbar main-navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="TEJI Logo" />
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <Link className="navbar-item" to="/mission/">
-          About
-        </Link>
-        <Link className="navbar-item" to="/projects/">
-          Projects
-        </Link>
-        <Link className="navbar-item" to="/news/">
-          News
-        </Link>
-        <Link className="navbar-item" to="/blog/">
-          Blog
-        </Link>
-        <Link className="navbar-item" to="/contact/">
-          Contact
-        </Link>
-      </div>
-    </div>
-  </nav>
-)
+
+class Navbar extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      menu: ''
+    }
+  }
+
+  handleBurgerClick() {
+    let menu = (this.state.menu === 'is-active') ? '' : 'is-active'
+    this.setState({menu})
+  }
+
+  render() {
+    return (
+      <nav className="navbar main-navbar is-transparent">
+        <div className="container">
+          <div className="navbar-brand is-vertical-center">
+            <Link to="/" className="navbar-item">
+              <figure className="image">
+                <img src={logo} alt="TEJI Logo" />
+              </figure>
+            </Link>
+            <a
+              role="button"
+              className="navbar-burger"
+              data-target="navMenu"
+              aria-label="menu"
+              aria-expanded="false"
+              onClick={this.handleBurgerClick.bind(this)}>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+          <div className={`navbar-menu ${this.state.menu}`} id="navMenu">
+            <div className="navbar-end">
+              <Link className="navbar-item" to="/mission/">
+                About
+              </Link>
+              <Link className="navbar-item" to="/projects/">
+                Projects
+              </Link>
+              <Link className="navbar-item" to="/news/">
+                News
+              </Link>
+              <Link className="navbar-item" to="/blog/">
+                Blog
+              </Link>
+              <Link className="navbar-item" to="/contact/">
+                Contact
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
 
 export default Navbar
