75cec9d815aa9ae8a4964fd6b082cfb6b38248ca
Richard Kim <kimr@mit.edu>
2018-10-15 10:33:16 -0400

    adjust container width

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,11 @@ const Header = () => (
       <div className="container">
         <div className="box mission-box">
           <h4 className="title has-text-weight-light is-italic is-5 is-size-3-tablet">
-            Creating sustainable solutions to mass incarceration, social injustice, and barriers to reentry via education and technology.
+            Creating sustainable solutions to
+            <br />
+            mass incarceration, social injustice, and barriers to reentry
+            <br />
+            via education and technology.
           </h4>
         </div>
       </div>
